37671171355e4a7ca5aecc8c4a4268505f4f3dc0
pawelrus91 <18408679+pawelrus91@users.noreply.github.com>
2026-09-06 23:26:36 +0200

    Add Django CORS headers configuration

--- djangomono/settings.py
+++ djangomono/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Third-party apps
+    'corsheaders',
     'rest_framework',
 
     # Local apps
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',

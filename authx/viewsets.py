7dc3a1daeea6c2f6566ded3f1125ce25dbce7410
pawelrus91 <18408679+pawelrus91@users.noreply.github.com>
2026-09-19 20:08:03 +0200

    Logout user in delete users endpoint

--- authx/viewsets.py
+++ authx/viewsets.py
@@ -1,6 +1,10 @@
+from django.contrib.auth import get_user_model, logout
 from rest_framework.viewsets import ModelViewSet
-from django.contrib.auth import get_user_model
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from rest_framework.status import HTTP_204_NO_CONTENT
+
+from authx.permissions import IsOwnerUser
 
 from .serializers import UserSerializer
 
@@ -10,4 +14,10 @@ User = get_user_model()
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsOwnerUser]
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        logout(request)
+        self.perform_destroy(instance)
+        return Response(status=HTTP_204_NO_CONTENT)

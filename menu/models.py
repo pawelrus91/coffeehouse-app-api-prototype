a767124adc2f3323884cc2432e9706c497b0061f
pawelrus91 <18408679+pawelrus91@users.noreply.github.com>
2026-09-23 02:54:22 +0200

    Create Component model in menu app

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Component(models.Model):
+    ingredient = models.ForeignKey(
+        'story.Ingredient',
+        on_delete=models.CASCADE,
+    )
+    quantity = models.IntegerField(default=1)
+
+    @property
+    def name(self):
+        return f"{self.ingredient.name} - {self.quantity} {self.ingredient.get_unit_display()}"
+
+    def __str__(self):
+        return self.name

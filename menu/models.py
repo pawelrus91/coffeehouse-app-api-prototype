3597e31df85ce7aaaf3037a05b1f17106901118d
pawelrus91 <18408679+pawelrus91@users.noreply.github.com>
2026-09-26 21:50:38 +0200

    Create MenuItem model in Menu app

--- menu/models.py
+++ menu/models.py
@@ -16,3 +16,20 @@ class Component(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    price_net = models.DecimalField(max_digits=6, decimal_places=2)
+    ingredients = models.ManyToManyField(
+        Component,
+        blank=True,
+    )
+    tax = models.DecimalField(max_digits=4, decimal_places=2, default=23)
+
+    @property
+    def price_gross(self):
+        return "{:.2f}".format(self.price_net * self.tax/100 + self.price_net)
+
+    def __str__(self):
+        return f"{self.name} {self.price_gross} PLN"
